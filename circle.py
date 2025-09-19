216fa1ada2d9dcb1be93753b5ff956fbe0fd977c
ok10010 <ok10010@yandex.ru>
2025-09-19 11:50:35 +0300

    Добавление комментариев о работе функций

--- circle.py
+++ circle.py
@@ -1,10 +1,35 @@
 import math
 
 
-def area(r):
+def area(r: float) -> float:
+    """
+    Вычисляет площадь круга по радиусу.
+
+    Параметры:
+        r (float): радиус круга.
+
+    Возвращает:
+        float: площадь круга.
+
+    Пример вызова:
+    >>> area(5)
+    78.53981633974483
+    """
     return math.pi * r * r
 
 
-def perimeter(r):
-    return 2 * math.pi * r
+def perimeter(r: float) -> float:
+    """
+    Вычисляет длину окружности по радиусу.
 
+    Параметры:
+        r (float): радиус круга.
+
+    Возвращает:
+        float: длина окружности.
+
+    Пример вызова:
+    >>> perimeter(5)
+    31.41592653589793
+    """
+    return 2 * math.pi * r
